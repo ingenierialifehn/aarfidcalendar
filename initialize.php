6e46e35c347f68110022397b6dd45fe2949101c9
ingenierialifehn <77239250+ingenierialifehn@users.noreply.github.com>
2023-10-16 09:52:50 -0600

    Add files via upload

---
+++ initialize.php
@@ -0,0 +1,10 @@
+<?php
+$dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'','password'=>'4b67deeb9aba04a5b54632ad19934f26','last_login'=>'','date_updated'=>'','date_added'=>'');
+if(!defined('base_url')) define('base_url','https://ingenierialife.com/calendar-absence/');
+if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+if(!defined('dev_data')) define('dev_data',$dev_data);
+if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"u942804061_puchocancha_d");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"Admin2022_$");
+if(!defined('DB_NAME')) define('DB_NAME',"u942804061_puchocancha_d");
+?>
